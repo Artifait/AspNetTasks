138f1c0b40ed63e10a25cd93491712e30fadc41a
Artifait <am3044288@gmail.com>
2025-03-17 16:04:29 +0700

    Update FRONTEND

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,12 +1,27 @@
-using Microsoft.AspNetCore.Mvc;
+using AspNetTasks.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspNetTasks.Pages
 {
-    public class IndexModel : PageModel
+    public class FilmScheduleModel : PageModel
     {
-        public void OnGet()
+        private readonly CinemaContext _context;
+
+        public FilmScheduleModel(CinemaContext context)
         {
+            _context = context;
+        }
+
+        // Список фильмов с сеансами
+        public List<Film> Films { get; set; } = new List<Film>();
+
+        public async Task OnGetAsync()
+        {
+            // Загрузка фильмов с их сеансами
+            Films = await _context.Films
+                .Include(f => f.Sessions)
+                .ToListAsync();
         }
     }
 }
