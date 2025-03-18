d8159f131cf89e4f16de2246a4e68affff429b83
Artifait <am3044288@gmail.com>
2025-03-18 21:56:27 +0700

    IgnoreAntiforgeryToken KRINGEEE

--- Pages/DeleteFilm.cshtml.cs
+++ Pages/DeleteFilm.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AspNetTasks.Pages
 {
+    [IgnoreAntiforgeryToken]
     public class DeleteFilmModel : PageModel
     {
         private readonly IFilmRepository _context;
--- Pages/DeleteSession.cshtml.cs
+++ Pages/DeleteSession.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AspNetTasks.Pages
 {
+    [IgnoreAntiforgeryToken]
     public class DeleteSessionModel : PageModel
     {
         private readonly IFilmRepository _context;
